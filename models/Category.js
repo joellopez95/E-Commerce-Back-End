24c299ea5101e19816a311a4ee2162c2c1a5ec37
Joel Lopez <jolopez.satx@gmail.com>
2023-11-19 22:26:16 -0600

    category.js and index.js code, added columns for cat, belongsTo code for index

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //copied from previous lesson, id is boiler plate code
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //checked category-seeds to identify name
+    category_name: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
